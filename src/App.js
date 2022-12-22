1a2c7fe0bafe6b2412c6be4c0d4f742b499baf2c
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-12-23 01:52:27 +0900

    updates routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
+import Experiences from './components/Experiences/Experiences';
+import Contact from './components/Contact/Contact';
+import About from './components/About/About';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Navigate to ='/' />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/experiences' element={<Experiences />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
