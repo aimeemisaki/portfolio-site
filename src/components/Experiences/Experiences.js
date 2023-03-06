fa42303f547473beae74464fe0f05470debaae45
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2023-03-05 19:07:10 -0800

    updates Experiences.js

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -2,16 +2,35 @@ import { useState } from "react";
 import toggleModal from "../../data-and-functions/toggleModal";
 import City from "./City";
 import Paris from './Paris'
+import worldMap from '../../assets/worldMap.png'
+import resume from '../../assets/resume.pdf'
+import Modal from 'react-bootstrap/Modal';
 
 const Experiences = () => {
 
     const [toggle, setToggle] = useState(false)
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
-        <div>
+        <div className='experiences' onClick={e => setToggle(toggleModal(e))}>
+            <div>
+                <button onClick={() => setToggle(toggleModal())}>
+                    RESUME
+                </button>
+            </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Body>
+                    <div>
+                    <iframe src={resume} title='resume' frameborder="0" width="100%" height="400px"> 
+                    </iframe>
+                    </div>
+                    </Modal.Body>
+            </Modal>
             <div className='experiences-container'>
-                <img className='world-map' src='https://i.ibb.co/9YdNz47/worldmap.png' alt='world-map'></img>
+                <img className='world-map' src={worldMap} alt='world-map'></img>
             </div>
             <Paris />
             
--- src/components/Experiences/Paris.js
+++ src/components/Experiences/Paris.js
@@ -2,20 +2,8 @@ import { useState } from "react";
 import toggleModal from "../../data-and-functions/toggleModal";
 
 function Paris() {
-    const [toggle, setToggle] = useState(false)
     return (
-        <div className='paris'>
-        <div className='paris-container' onClick={e => setToggle(toggleModal(e))}>
-            <h1>Paris, France</h1>
-        </div>
-        {toggle && (
-            <div className='modals'>
-                <div className='modals-content'>
-                    <p>hello</p>
-                </div>
-            </div>
-        )}
-        </div>
+       <></>
     )
 }
 
